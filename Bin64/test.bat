f5bae5715ef0c3cf93fc0e5c838038ec2f8cf2d8
simpson <hi_simpson@daum.net>
2019-05-20 00:31:07 +0900

    refs #0 run debug

--- Bin64/test.bat
+++ Bin64/test.bat
@@ -1,5 +1,8 @@
 
 ::DX11Viewer.exe http://html5test.com/
 
-DX11Viewer.exe file:///%~dp0/test.html
+::DX11Viewer.exe file:///%~dp0/test.html
+
+::DX11Viewer.debug.exe file:///D:/work_eawebkit/eawebkit_demo/Bin64/test.html
+DX11Viewer.debug.exe file:///%~dp0/test.html
 
